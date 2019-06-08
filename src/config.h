98fe3c359a546f49af0d222d644f785626a8aa7d
Alex Solomaha <cyanofresh@gmail.com>
2019-06-08 18:42:31 +0300

    changes for coursework

--- src/config.h
+++ src/config.h
@@ -7,13 +7,13 @@ namespace config {
     const char WIFI_SSID[] = "Solomaha";
     const char WIFI_PASSWORD[] = "solomakha21";
 
-    const auto MQTT_HOST = IPAddress(192, 168, 1, 230);
-//    const auto MQTT_HOST = IPAddress(176, 36, 198, 196);
+//    const auto MQTT_HOST = IPAddress(192, 168, 1, 230);
+    const auto MQTT_HOST = IPAddress(176, 36, 198, 196);
     const uint16_t MQTT_PORT = 1883;
     const char MQTT_ID[] = "balcony-meteo";
     const char MQTT_PASSWORD[] = "gfbnjdfgshbnmsdgbnhjmksgdfsdfgdhfdhgf";
 
-    const uint8_t SENSOR_READ_INTERVAL = 60;
+    const uint8_t SENSOR_READ_INTERVAL = 10;
     const uint8_t READINGS_COUNT = 3;
 }
 
